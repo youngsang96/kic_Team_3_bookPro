677927b6d76ab03422eac1405e72bb5fbf9f6c67
yohesa <fhqhtqlf159@gmail.com>
2021-09-11 23:07:20 +0900

    MemberDao complete

--- bookPro/src/DAO/MemberDao.java
+++ bookPro/src/DAO/MemberDao.java
@@ -1,6 +1,7 @@
 package DAO;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -11,10 +12,12 @@ import model.Member;
 import util.DBConnection;
 
 public class MemberDao {
-	public int memberInsert(Member member){
+	
+	//1. 테이블에 삽입
+	public int MemberInsert(Member member){
 		Connection con = DBConnection.getConnection();
-		
 		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		String sql = "insert into member values(?, ?, ?, ?, ?, ?, ?)";
 		
 		try {
@@ -22,17 +25,21 @@ public class MemberDao {
 			pstmt.setString(1, member.getMemberid());
 			pstmt.setString(2, member.getName());
 			pstmt.setString(3, member.getPass());
-			pstmt.setString(4, member.getBirthday());
-			pstmt.setInt(5, member.getGender());
+			pstmt.setString(4,  member.getBirthday());
+			pstmt.setString(5, member.getGender());
 			pstmt.setString(6, member.getTel());
 			pstmt.setString(7, member.getEmail());		
+			
+			return pstmt.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			DBConnection.close(con, pstmt, null);
+			DBConnection.close(con, pstmt, rs);
 		}
 		return 0;
 	}
+	
+	//2. 테이블에 있는 값 리스트로 저장
 	public List<Member> memberList(){
 		Connection con = DBConnection.getConnection();
 		PreparedStatement pstmt = null;
@@ -48,7 +55,7 @@ public class MemberDao {
 					member.setName(rs.getString("name"));
 					member.setPass(rs.getString("pass"));
 					member.setBirthday(rs.getString("birthday"));
-					member.setGender(rs.getInt("gender"));
+					member.setGender(rs.getString("gender"));
 					member.setTel(rs.getString("tel"));
 					member.setEmail(rs.getString("email"));
 					li.add(member);
@@ -59,22 +66,26 @@ public class MemberDao {
 				DBConnection.close(con, pstmt, null);
 			} return li;
 		}
+	
+	//sql테이블에 삽입된 값 member.java에 set
 	public Member selectOne(String memberid) {
 		Connection con = DBConnection.getConnection();
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+		
 		String sql = "select * from member where memberid=?";
 		Member member = new Member();
+		
 		try {
 				pstmt = con.prepareStatement(sql);
 				pstmt.setString(1, memberid);
 				rs = pstmt.executeQuery();
 				if(rs.next()) {
 					member.setMemberid(rs.getString("memberid"));
-					member.setName(rs.getString("name"));
 					member.setPass(rs.getString("pass"));
+					member.setName(rs.getString("name"));
 					member.setBirthday(rs.getString("birthday"));
-					member.setGender(rs.getInt("gender"));
+					member.setGender(rs.getString("gender"));
 					member.setTel(rs.getString("tel"));
 					member.setEmail(rs.getString("email"));
 				}	
@@ -83,8 +94,11 @@ public class MemberDao {
 			} finally {
 				DBConnection.close(con, pstmt, rs);
 			}
+		    System.out.println(member);
 			return member; 
 	}
+	
+	
 	public int MemberUpdate(Member member) {
 		Connection con = DBConnection.getConnection();
 		PreparedStatement pstmt = null;
@@ -94,8 +108,8 @@ public class MemberDao {
 		try {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, member.getName());
-			pstmt.setInt(2, member.getGender());
-			pstmt.setString(3, member.getBirthday());
+			pstmt.setString(2, member.getGender());
+			pstmt.setString(3,  member.getBirthday());
 			pstmt.setString(4, member.getTel());
 			pstmt.setString(5, member.getEmail());
 			pstmt.setString(6, member.getMemberid());
